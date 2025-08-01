02c9b1d08dbfbf84a7ea79b0aff34b55296e51ed
LabhanshPal <148335088+LabhanshPal@users.noreply.github.com>
2025-08-01 16:21:11 +0530

    Dehydration.py

--- final_project/dehydration.py
+++ final_project/dehydration.py
@@ -1,5 +1,5 @@
 import os
-# import cv2
+import cv2
 import shutil
 import numpy as np
 import matplotlib.pyplot as plt
@@ -36,8 +36,8 @@ def preprocess_images(input_folder, output_folder, img_size=(224, 224)):
     print(f"Processed {processed_count} images in {input_folder}")
 
 # Preprocess both classes
-# preprocess_images("Dataset/Healthy_Infants", "Preprocessed/Healthy_Infants")
-# preprocess_images("Dataset/Dehydrated_Infants", "Preprocessed/Dehydrated_Infants")
+preprocess_images("Dataset/Healthy_Infants", "Preprocessed/Healthy_Infants")
+preprocess_images("Dataset/Dehydrated_Infants", "Preprocessed/Dehydrated_Infants")
 
 # Dataset splitting
 INPUT_DIR = "Preprocessed"
